60b5823107017f11d1e95f5cc71e8aca572e6383
Bird <birdmandayum0131@gmail.com>
2024-10-14 14:09:13 +0800

    feat: add custom_id to button
    - add custom_id to achieve persistent view

--- button.py
+++ button.py
@@ -3,9 +3,19 @@ import discord
 
 class StartServerButton(discord.ui.Button):
     def __init__(self, disabled: bool = False):
-        super().__init__(label="Start Minecraft Server", style=discord.ButtonStyle.green, disabled=disabled)
+        super().__init__(
+            label="Start Minecraft Server",
+            style=discord.ButtonStyle.green,
+            disabled=disabled,
+            custom_id="start_server_button",
+        )
 
 
 class StopServerButton(discord.ui.Button):
     def __init__(self, disabled: bool = False):
-        super().__init__(label="Stop Minecraft Server", style=discord.ButtonStyle.grey, disabled=disabled)
+        super().__init__(
+            label="Stop Minecraft Server",
+            style=discord.ButtonStyle.grey,
+            disabled=disabled,
+            custom_id="stop_server_button",
+        )
